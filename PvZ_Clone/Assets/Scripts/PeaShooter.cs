456609e220b8269fb4727b5cbd65a677a198fea0
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-04-26 10:58:03 +0300

    Some prefabs and minor scene mods

---
+++ PvZ_Clone/Assets/Scripts/PeaShooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeaShooter : MonoBehaviour {
+
+    public GameObject ammo;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(ammo);
+            transform.position = ammo.transform.position;
+            ammo.transform.Translate(1, 2, 0);
+        }
+		
+	}
+}

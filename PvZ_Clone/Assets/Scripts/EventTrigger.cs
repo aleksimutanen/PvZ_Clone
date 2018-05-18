12bf1a5e1797217398c8bb85ae5904ef8adf3ec8
kristiina misikangas <a.b@c.d>
2018-05-18 12:43:30 +0300

    ...

---
+++ PvZ_Clone/Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour {
+
+    public string eventinNimi;
+    public string Music;
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Fabric.EventManager.Instance.PostEvent(eventinNimi);
+        }
+        if (Input.GetKeyDown(KeyCode.P)) {
+            Fabric.EventManager.Instance.PostEvent(Music);
+        }
+    }
+}

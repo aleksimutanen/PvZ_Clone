08f6ad4ac2f8ce0dacb5979d275d513b7bc327ef
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-05-21 14:50:39 +0300

    "Last chance" emp added

---
+++ PvZ_Clone/Assets/Scripts/EMPWave.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMPWave : MonoBehaviour {
+
+    public float empDamage;
+    float empDuration = 2f;
+
+    void Update() {
+        empDuration -= Time.deltaTime;
+        if (empDuration < 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        var b = collision.gameObject.GetComponent<Bot>();
+        b.TakeDamage(empDamage);
+        print("Emp active");
+    }
+}
+

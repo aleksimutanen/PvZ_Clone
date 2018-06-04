6ca55f42122188966f89694d3b3900b4e10a08fd
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-06-04 15:37:06 +0300

    swattermode almost complete

---
+++ PvZ_Clone/Assets/Scripts/PersistentDataStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentDataStorage : MonoBehaviour {
+
+    public float highScore;
+
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+}
